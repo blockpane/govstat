0b4f8f64f9fd5d72ae6ba5af5452c1036ba82fe2
Todd <todd@blockpane.com>
2023-04-12 14:11:41 -0600

    initial commit

---
+++ index.md
@@ -0,0 +1,74 @@
+# govstat
+
+A very simple tool to query multiple chains for governance proposals.
+
+The config is loaded from a file called `chains.yml` in the current working directory.
+The format for this file is simple:
+
+```yaml
+---
+
+# update the following with your own chain IDs, RPC endpoint, and validator addresses
+# repeat as needed for each chain you want to monitor
+
+chains:
+- chain_id: "akashnet-2"
+  validator: "akash1zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+  node: "http://somehost:26657"
+- chain_id: "injective-1"
+  validator: "inj1zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+  node: "http://someotherhost:26657"
+```
+
+It will query the RPC endpoint for governance proposals in the "voting_period" state showing
+the status of the validator's vote on each proposal.
+A red ❌ indicates the validator has not voted on the proposal, a green ✅ indicates the validator has.
+
+## Example output:
+
+```text
+$ govstat
+
+* found 2 proposals for akashnet-2
+✅ proposal 199 ends: 2023-04-17 17:51:38
+✅ proposal 200 ends: 2023-04-17 17:56:50
+
+* found 2 proposals for injective-1
+✅ proposal 218 ends: 2023-04-14 06:16:45
+✅ proposal 219 ends: 2023-04-14 07:38:26
+
+* found 2 proposals for osmosis-1
+✅ proposal 481 ends: 2023-04-15 09:37:29
+❌ proposal 482 ends: 2023-04-17 09:07:18
+
+* no proposals on jackal-1
+
+* found 1 proposals for juno-1
+✅ proposal 282 ends: 2023-04-15 13:55:15
+
+* found 1 proposals for kava_2222-10
+✅ proposal 135 ends: 2023-04-13 16:26:39
+
+* no proposals on mars-1
+
+* no proposals on migaloo-1
+
+* found 2 proposals for secret-4
+✅ proposal 233 ends: 2023-04-18 06:59:47
+✅ proposal 235 ends: 2023-04-19 04:28:05
+
+* found 4 proposals for stargaze-1
+✅ proposal 155 ends: 2023-04-12 18:34:49
+✅ proposal 156 ends: 2023-04-14 07:54:31
+✅ proposal 157 ends: 2023-04-14 09:19:52
+✅ proposal 158 ends: 2023-04-14 09:22:00
+
+* no proposals on nois-1
+
+```
+
+## Installation
+
+```bash
+$ go install github.com/blockpane/govstat@latest
+```
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/types/query"
+	gov "github.com/cosmos/cosmos-sdk/x/gov/types"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+)
+
+type Chain struct {
+	ChainID   string `yaml:"chain_id"`
+	Validator string `yaml:"validator"`
+	Node      string `yaml:"node"`
+}
+
+type chains struct {
+	Chains []Chain `yaml:"chains"`
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	chainsYaml, err := os.ReadFile("chains.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chains := &chains{}
+	err = yaml.Unmarshal(chainsYaml, chains)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, chain := range chains.Chains {
+		//log.Println("checking", chain.ChainID)
+		client, err := rpchttp.New(chain.Node, "/websocket")
+		if err != nil {
+			log.Printf("error connecting to %s: %s\n", chain.ChainID, err)
+			continue
+		}
+
+		status, err := client.Status(context.Background())
+		if err != nil {
+			log.Printf("error getting status %s\n", err)
+			continue
+		}
+		if status.NodeInfo.Network != chain.ChainID {
+			log.Printf("chain id mismatch %s != %s\n", status.NodeInfo.Network, chain.ChainID)
+			continue
+		}
+
+		qpr := gov.QueryProposalsRequest{
+			ProposalStatus: gov.StatusVotingPeriod,
+			//Voter:          "",
+			//Depositor:      "",
+			Pagination: &query.PageRequest{Limit: 100, Offset: 0},
+		}
+
+		q, err := qpr.Marshal()
+		if err != nil {
+			log.Printf("error marshaling %s\n", err)
+			continue
+		}
+
+		pResp, err := client.ABCIQuery(context.Background(), "/cosmos.gov.v1beta1.Query/Proposals", q)
+		if err != nil {
+			log.Printf("error getting votes %s\n", err)
+			continue
+		}
+		response := &gov.QueryProposalsResponse{}
+		err = response.Unmarshal(pResp.Response.Value)
+		if err != nil {
+			log.Printf("error unmarshaling %s\n", err)
+			continue
+		}
+
+		if len(response.Proposals) == 0 {
+			fmt.Print("* no proposals on ", chain.ChainID, "\n\n")
+			continue
+		}
+		fmt.Printf("* found %d proposals for %s\n", len(response.Proposals), chain.ChainID)
+		for _, proposal := range response.Proposals {
+			qr := gov.QueryVoteRequest{
+				ProposalId: proposal.ProposalId,
+				Voter:      chain.Validator,
+			}
+			q, err := qr.Marshal()
+			if err != nil {
+				log.Printf("error marshaling %s\n", err)
+				continue
+			}
+			pResp, err = client.ABCIQuery(context.Background(), "/cosmos.gov.v1beta1.Query/Vote", q)
+			if err != nil {
+				log.Printf("error getting vote %s\n", err)
+				continue
+			}
+			response := &gov.QueryVoteResponse{}
+			err = response.Unmarshal(pResp.Response.Value)
+			if err != nil {
+				log.Printf("error unmarshaling %s\n", err)
+				continue
+			}
+			voted := "❌"
+			if response.Vote.Voter == chain.Validator {
+				voted = "✅"
+			}
+			fmt.Printf("%s proposal %d ends: %s\n", voted, proposal.ProposalId, proposal.VotingEndTime.Local().Format("2006-01-02 15:04:05"))
+		}
+		fmt.Println("")
+	}
+}
