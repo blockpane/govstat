1b5b9782e0004be4f5b0ca1cbe29034d2655e98e
Todd <todd@blockpane.com>
2023-07-20 18:26:49 -0600

    should now handle both v1 and v1beta1 for x/gov

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"log"
 	"os"
 
+	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
 	"github.com/cosmos/cosmos-sdk/types/query"
-	gov "github.com/cosmos/cosmos-sdk/x/gov/types"
-	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+	gov1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	gov1b1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 )
 
 type Chain struct {
@@ -37,7 +38,6 @@ func main() {
 	}
 
 	for _, chain := range chains.Chains {
-		//log.Println("checking", chain.ChainID)
 		client, err := rpchttp.New(chain.Node, "/websocket")
 		if err != nil {
 			log.Printf("error connecting to %s: %s\n", chain.ChainID, err)
@@ -54,11 +54,10 @@ func main() {
 			continue
 		}
 
-		qpr := gov.QueryProposalsRequest{
-			ProposalStatus: gov.StatusVotingPeriod,
-			//Voter:          "",
-			//Depositor:      "",
-			Pagination: &query.PageRequest{Limit: 100, Offset: 0},
+		// this seems to marshal the same on v1 and v1beta1
+		qpr := gov1b1.QueryProposalsRequest{
+			ProposalStatus: gov1b1.StatusVotingPeriod,
+			Pagination:     &query.PageRequest{Limit: 100, Offset: 0},
 		}
 
 		q, err := qpr.Marshal()
@@ -67,57 +66,110 @@ func main() {
 			continue
 		}
 
-		response := &gov.QueryProposalsResponse{}
-		var prefix string
-		for _, path := range []string{"/cosmos.gov.v1beta1.Query", "/cosmos.gov.v1.Query"} {
-			pResp, err := client.ABCIQuery(context.Background(), path+"/Proposals", q)
+		response1b1 := &gov1b1.QueryProposalsResponse{}
+		response1 := &gov1.QueryProposalsResponse{}
+		var path string
+		var props int
+		for _, path = range []string{"v1", "v1beta1"} {
+			pResp, err := client.ABCIQuery(context.Background(), fmt.Sprintf("/cosmos.gov.%s.Query/Proposals", path), q)
 			if err != nil {
 				log.Printf("error getting votes %s\n", err)
 				continue
 			}
-			err = response.Unmarshal(pResp.Response.Value)
+			switch path {
+			case "v1beta1":
+				err = response1b1.Unmarshal(pResp.Response.Value)
+			case "v1":
+				err = response1.Unmarshal(pResp.Response.Value)
+			}
 			if err != nil {
 				log.Printf("error unmarshaling %s\n", err)
 				continue
 			}
-			if len(response.Proposals) != 0 {
-				prefix = path
+			switch path {
+			case "v1beta1":
+				props = len(response1b1.Proposals)
+			case "v1":
+				props = len(response1.Proposals)
+			}
+			if props != 0 {
 				break
 			}
 		}
-
-		if len(response.Proposals) == 0 {
+		if props == 0 {
 			fmt.Print("* no proposals on ", chain.ChainID, "\n\n")
 			continue
 		}
-		fmt.Printf("* found %d proposals for %s\n", len(response.Proposals), chain.ChainID)
-		for _, proposal := range response.Proposals {
-			qr := gov.QueryVoteRequest{
-				ProposalId: proposal.ProposalId,
-				Voter:      chain.Validator,
-			}
-			q, err := qr.Marshal()
-			if err != nil {
-				log.Printf("error marshaling %s\n", err)
-				continue
-			}
-			pResp, err := client.ABCIQuery(context.Background(), prefix+"/Vote", q)
-			if err != nil {
-				log.Printf("error getting vote %s\n", err)
-				continue
-			}
-			response := &gov.QueryVoteResponse{}
-			err = response.Unmarshal(pResp.Response.Value)
-			if err != nil {
-				log.Printf("error unmarshaling %s\n", err)
-				continue
-			}
-			voted := "❌"
-			if response.Vote.Voter == chain.Validator {
-				voted = "✅"
-			}
-			fmt.Printf("%s proposal %d ends: %s\n", voted, proposal.ProposalId, proposal.VotingEndTime.Local().Format("2006-01-02 15:04:05"))
+		fmt.Printf("* found %d proposals for %s\n", props, chain.ChainID)
+		switch path {
+		case "v1beta1":
+			v1b1Response(response1b1, client, chain)
+		case "v1":
+			v1Response(response1, client, chain)
 		}
-		fmt.Println("")
 	}
+}
+
+func v1Response(response *gov1.QueryProposalsResponse, client *rpchttp.HTTP, chain Chain) {
+	for _, proposal := range response.Proposals {
+		qr := gov1.QueryVoteRequest{
+			ProposalId: proposal.Id,
+			Voter:      chain.Validator,
+		}
+		q, err := qr.Marshal()
+		if err != nil {
+			log.Printf("error marshaling %s\n", err)
+			continue
+		}
+		pResp, err := client.ABCIQuery(context.Background(), "/cosmos.gov.v1.Query/Vote", q)
+		if err != nil {
+			log.Printf("error getting vote %s\n", err)
+			continue
+		}
+		// fmt.Println(len(pResp.Response.Value))
+		response := &gov1.QueryVoteResponse{}
+		err = response.Unmarshal(pResp.Response.Value)
+		if err != nil {
+			log.Printf("error unmarshaling %s\n", err)
+			continue
+		}
+		voted := "❌"
+		if response.Vote != nil && response.Vote.Voter == chain.Validator {
+			voted = "✅"
+		}
+		fmt.Printf("%s proposal %d ends: %s\n", voted, proposal.Id, proposal.VotingEndTime.Local().Format("2006-01-02 15:04:05"))
+	}
+	fmt.Println("")
+}
+
+func v1b1Response(response *gov1b1.QueryProposalsResponse, client *rpchttp.HTTP, chain Chain) {
+	for _, proposal := range response.Proposals {
+		qr := gov1b1.QueryVoteRequest{
+			ProposalId: proposal.ProposalId,
+			Voter:      chain.Validator,
+		}
+		q, err := qr.Marshal()
+		if err != nil {
+			log.Printf("error marshaling %s\n", err)
+			continue
+		}
+		pResp, err := client.ABCIQuery(context.Background(), "/cosmos.gov.v1beta1.Query/Vote", q)
+		if err != nil {
+			log.Printf("error getting vote %s\n", err)
+			continue
+		}
+		// fmt.Println(len(pResp.Response.Value))
+		response := &gov1b1.QueryVoteResponse{}
+		err = response.Unmarshal(pResp.Response.Value)
+		if err != nil {
+			log.Printf("error unmarshaling %s\n", err)
+			continue
+		}
+		voted := "❌"
+		if response.Vote.Voter == chain.Validator {
+			voted = "✅"
+		}
+		fmt.Printf("%s proposal %d ends: %s\n", voted, proposal.ProposalId, proposal.VotingEndTime.Local().Format("2006-01-02 15:04:05"))
+	}
+	fmt.Println("")
 }
